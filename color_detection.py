bc7c3346095165732b0f5aa8a5b732b9ab7b6798
Ajinkya Mahalpure <ajinkya.mangesh2020@vitbhopal.ac.in>
2024-03-03 16:09:56 +0530

    Updated .py

--- color_detection.py
+++ color_detection.py
@@ -3,70 +3,55 @@ import numpy as np
 import pandas as pd
 import argparse
 
-#Creating argument parser to take image path from command line
-ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True, help="Image Path")
-args = vars(ap.parse_args())
-img_path = args['image']
+# Creating argument parser to take image path from command line
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('-i', '--image', required=True, help="Image Path")
+arguments = vars(arg_parser.parse_args())
+image_path = arguments['image']
 
-#Reading the image with opencv
-img = cv2.imread(img_path)
+# Reading the image with OpenCV
+image = cv2.imread(image_path)
 
-#declaring global variables (are used later on)
+# Declaring global variables (used later on)
 clicked = False
-r = g = b = xpos = ypos = 0
+r_value = g_value = b_value = x_pos = y_pos = 0
 
-#Reading csv file with pandas and giving names to each column
-index=["color","color_name","hex","R","G","B"]
-csv = pd.read_csv('colors.csv', names=index, header=None)
+# Reading CSV file with Pandas and providing names to each column
+column_names = ["color", "color_name", "hex_code", "Red", "Green", "Blue"]
+color_data = pd.read_csv('colors.csv', names=column_names, header=None)
 
-#function to calculate minimum distance from all colors and get the most matching color
-def getColorName(R,G,B):
-    minimum = 10000
-    for i in range(len(csv)):
-        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
-        if(d<=minimum):
-            minimum = d
-            cname = csv.loc[i,"color_name"]
-    return cname
+# Function to calculate minimum distance from all colors and get the most matching color
+def get_color_name(red, green, blue):
+    min_distance = 10000
+    for i in range(len(color_data)):
+        distance = abs(red - int(color_data.loc[i, "Red"])) + abs(green - int(color_data.loc[i, "Green"])) + abs(blue - int(color_data.loc[i, "Blue"]))
+        if distance <= min_distance:
+            min_distance = distance
+            color_name = color_data.loc[i, "color_name"]
+    return color_name
 
-#function to get x,y coordinates of mouse double click
-def draw_function(event, x,y,flags,param):
+# Function to get x, y coordinates of mouse double click
+def handle_mouse_event(event, x, y, flags, param):
+    global b_value, g_value, r_value, x_pos, y_pos, clicked
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        global b,g,r,xpos,ypos, clicked
         clicked = True
-        xpos = x
-        ypos = y
-        b,g,r = img[y,x]
-        b = int(b)
-        g = int(g)
-        r = int(r)
-       
+        x_pos = x
+        y_pos = y
+        b_value, g_value, r_value = image[y, x]
+
 cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_function)
+cv2.setMouseCallback('image', handle_mouse_event)
 
-while(1):
-
-    cv2.imshow("image",img)
-    if (clicked):
-   
-        #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
-        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
-
-        #Creating text string to display( Color name and RGB values )
-        text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
-        
-        #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
-
-        #For very light colours we will display text in black colour
-        if(r+g+b>=600):
-            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
-            
-        clicked=False
-
-    #Break the loop when user hits 'esc' key    
-    if cv2.waitKey(20) & 0xFF ==27:
+while True:
+    cv2.imshow("image", image)
+    if clicked:
+        cv2.rectangle(image, (20, 20), (750, 60), (int(b_value), int(g_value), int(r_value)), -1)
+        color_text = get_color_name(r_value, g_value, b_value) + ' R=' + str(r_value) + ' G=' + str(g_value) + ' B=' + str(b_value)
+        cv2.putText(image, color_text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+        if r_value + g_value + b_value >= 600:
+            cv2.putText(image, color_text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+        clicked = False
+    if cv2.waitKey(20) & 0xFF == 27:
         break
-    
+
 cv2.destroyAllWindows()
